7e990e28cf28916cceba81228dfce4500de37e7c
Biplab <biplabshankarsamal@gmail.com>
2026-02-04 05:26:07 +0530

    Switch to Postgres

--- backend/settings.py
+++ backend/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import dj_database_url
+import os
 
 from pathlib import Path
 
@@ -78,10 +80,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # This tells Django: "If there is a DATABASE_URL, use it. Otherwise, use local SQLite."
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
